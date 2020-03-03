bb7244c663647454df5dbeceb48dc05e8c302efd
kozzztya <kozzztya@gmail.com>
2020-03-03 06:18:02 +0200

    Add Material UI Add basic responsiveness

--- src/components/RandomAnswerer/RandomAnswerer.js
+++ src/components/RandomAnswerer/RandomAnswerer.js
@@ -4,30 +4,42 @@ import titles from "../../titles";
 import PropTypes from "prop-types";
 
 import styles from "./RandomAnswerer.module.scss";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
 
 export const RandomAnswerer = props => {
   const wantsToAnswer = _.sample(props.answerers);
   const randomTitle = _.sample(titles);
 
   return (
-    <>
-      <h2>{randomTitle}</h2>
-      <span id="answerer" className={styles.hide}>
-        {wantsToAnswer.name}
-      </span>
-      <button id="show-button" onClick={show}>
-        Показать
-      </button>
-    </>
+    <Card className={styles.container}>
+      <CardContent align="center">
+        <Typography variant="h6" className={styles.title}>{randomTitle}</Typography>
+
+        <div className={styles.answererContainer}>
+          <div id="random-answerer" className={styles.hide}>
+            <Typography color="textSecondary">{wantsToAnswer.name}</Typography>
+          </div>
+
+          <div id="show-button">
+            <Button size="small" onClick={show}>
+              Показать
+            </Button>
+          </div>
+        </div>
+      </CardContent>
+    </Card>
   );
 };
 
 function show() {
-  const answerer = document.getElementById("answerer");
-  answerer.className = "";
+  const nameElement = document.getElementById("random-answerer");
+  nameElement.className = "";
 
-  const showButton = document.getElementById("show-button");
-  showButton.className = styles.hide;
+  const buttonElement = document.getElementById("show-button");
+  buttonElement.className = styles.hide;
 }
 
 RandomAnswerer.propTypes = {
--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App/App";
+import { ThemeProvider } from "./ThemeProvider";
 
 import "./index.scss";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <ThemeProvider>
+    <App />
+  </ThemeProvider>,
+  document.getElementById("root")
+);
