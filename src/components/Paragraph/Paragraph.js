c68ed6b3bb74b955ce645abe0f1abe236b9e8afc
kozzztya <kozzztya@gmail.com>
2020-03-12 11:48:27 +0200

    Split words in render

--- src/components/Paragraph/Paragraph.js
+++ src/components/Paragraph/Paragraph.js
@@ -2,23 +2,21 @@ import React from "react";
 import faker from "faker";
 
 export class Paragraph extends React.Component {
-  state = {
-    words: this.props.text.split(" ").map(word => {
-      return {
-        word,
-        id: faker.random.uuid()
-      };
-    })
-  };
-
   shouldComponentUpdate(nextProps, nextState, nextContext) {
     return nextProps.src !== this.props.src;
   }
 
   render() {
+    const words = this.props.text.split(" ").map(word => {
+      return {
+        word,
+        id: faker.random.uuid()
+      };
+    });
+
     return (
       <div>
-        {this.state.words.map(({ id, word }) => (
+        {words.map(({ id, word }) => (
           <span key={id}>
             <span>{word}</span>
             <span> </span>
