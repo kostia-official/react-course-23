5c03f21d76f51a720f17d70a979d07d0d3e7b468
kozzztya <kozzztya@gmail.com>
2020-03-18 13:58:01 +0200

    Center error message

--- src/components/UnauthorizedErrorMessage/UnauthorizedErrorMessage.js
+++ src/components/UnauthorizedErrorMessage/UnauthorizedErrorMessage.js
@@ -1,12 +1,13 @@
 import React from "react";
+import { CenterText } from "../CenterText/CenterText";
 
 export const UnauthorizedErrorMessage = () => (
-  <>
+  <CenterText>
     <div>
       <span>Запрос успешно зафейлился</span>
     </div>
     <div>
       <span>У тебя есть доступ только на чтение</span>
     </div>
-  </>
+  </CenterText>
 );
