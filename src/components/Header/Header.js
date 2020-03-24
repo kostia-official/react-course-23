244422c9d9bf1502adb359a95a278e496433b4e1
kozzztya <kozzztya@gmail.com>
2020-03-24 19:20:19 +0200

    Add lessons and attendance pages

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,11 @@
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
+import { Menu, ChevronLeft } from '@material-ui/icons';
 import Typography from '@material-ui/core/Typography';
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
+import { useHistory } from 'react-router-dom';
 
 const useStyles = makeStyles((theme) => ({
   menuButton: {
@@ -12,14 +13,27 @@ const useStyles = makeStyles((theme) => ({
   }
 }));
 
-export const Header = () => {
+export const Header = ({ onMenuClick }) => {
   const classes = useStyles();
+  const history = useHistory();
+
+  const isShowBack = history.location.pathname !== '/';
+
+  const onBackClick = () => {
+    history.goBack();
+  };
 
   return (
     <AppBar position="static">
       <Toolbar>
-        <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-          <MenuIcon />
+        <IconButton
+          edge="start"
+          className={classes.menuButton}
+          color="inherit"
+          aria-label="menu"
+          onClick={isShowBack ? onBackClick : onMenuClick}
+        >
+          {isShowBack ? <ChevronLeft /> : <Menu />}
         </IconButton>
         <Typography variant="h6">Кто Хочет Ответить</Typography>
       </Toolbar>
---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Typography from '@material-ui/core/Typography';
+import { CenterText } from '../CenterText/CenterText';
+
+export class NotFound extends React.Component {
+  render() {
+    return (
+      <CenterText>
+        <img src="https://media3.giphy.com/media/RHzqdZJztOu7S/source.gif" alt="Not found" />
+        <Typography>Страница не найдена</Typography>
+      </CenterText>
+    );
+  }
+}
