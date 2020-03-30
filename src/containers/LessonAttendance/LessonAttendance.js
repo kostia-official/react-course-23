752e7e127ceac63dbb8103153bbec9c22b95f027
kozzztya <kozzztya@gmail.com>
2020-03-30 10:33:08 +0300

    Refactor LessonAttendance to functional component

--- src/containers/LessonAttendance/LessonAttendance.js
+++ src/containers/LessonAttendance/LessonAttendance.js
@@ -4,20 +4,18 @@ import { StudentsList } from '../../components/StudentsList/StudentsList';
 import qs from 'query-string';
 import GetStudents from '../../components/GetStudents/GetStudents';
 
-class LessonAttendance extends React.Component {
-  render() {
-    const { date } = qs.parse(this.props.location.search);
+function LessonAttendance({ location }) {
+  const { date } = qs.parse(location.search);
 
-    return (
-      <GetStudents date={date}>
-        {(students) => {
-          const presentStudents = _.filter(students, 'isPresent');
+  return (
+    <GetStudents date={date}>
+      {(students) => {
+        const presentStudents = _.filter(students, 'isPresent');
 
-          return <StudentsList students={presentStudents} />;
-        }}
-      </GetStudents>
-    );
-  }
+        return <StudentsList students={presentStudents} />;
+      }}
+    </GetStudents>
+  );
 }
 
 export default LessonAttendance;
