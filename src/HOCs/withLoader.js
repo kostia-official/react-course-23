cc5e1d57149947857853f2ba84e1fc403f869901
Kostiantyn Zvonilov <kozzztya@gmail.com>
2020-03-31 09:32:23 +0300

    Refactor code with HOCs and render props (#10)

---
+++ src/HOCs/withLoader.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Spinner } from '../components/Spinner/Spinner';
+import styled from 'styled-components';
+
+const ToHide = styled.div`
+  display: ${({ isShow }) => (isShow ? 'block' : 'none')};
+`;
+
+export const withLoader = (WrappedComponent) => {
+  return class extends React.Component {
+    state = {
+      isLoading: true
+    };
+
+    setIsLoading = (isLoading) => {
+      this.setState({ isLoading });
+    };
+
+    render() {
+      return (
+        <div>
+          <ToHide isShow={this.state.isLoading}>
+            <Spinner />
+          </ToHide>
+
+          <ToHide isShow={!this.state.isLoading}>
+            <WrappedComponent {...this.props} setIsLoading={this.setIsLoading} />
+          </ToHide>
+        </div>
+      );
+    }
+  };
+};
--- src/containers/LessonAttendance/LessonAttendance.js
+++ src/containers/LessonAttendance/LessonAttendance.js
@@ -1,68 +1,21 @@
 import React from 'react';
 import _ from 'lodash';
-import { UnauthorizedErrorMessage } from '../../components/UnauthorizedErrorMessage/UnauthorizedErrorMessage';
-import { Spinner } from '../../components/Spinner/Spinner';
-import { ErrorMessage } from '../../components/ErrorMessage/ErrorMessage';
-import { getStudents } from '../../api';
 import { StudentsList } from '../../components/StudentsList/StudentsList';
 import qs from 'query-string';
+import { GetStudents } from '../../components/GetStudents/GetStudents';
 
-class LessonAttendance extends React.Component {
-  state = {
-    students: [],
-    isLoading: true,
-    errorMessage: ''
-  };
+function LessonAttendance({ location }) {
+  const { date } = qs.parse(location.search);
 
-  async componentDidMount() {
-    try {
-      this.setState({ isLoading: true });
+  return (
+    <GetStudents date={date}>
+      {(students) => {
+        const presentStudents = _.filter(students, 'isPresent');
 
-      const { date } = qs.parse(this.props.location.search);
-      const students = await getStudents(date);
-      const presentStudents = _.filter(students, 'isPresent');
-
-      this.setState({ students: presentStudents });
-    } catch (err) {
-      this.setErrorMessage(err);
-    } finally {
-      this.setState({ isLoading: false });
-    }
-  }
-
-  setErrorMessage = async (err) => {
-    const isUnauthorized = _.get(err, 'response.status') === 401;
-
-    const errorMessage = isUnauthorized ? (
-      <UnauthorizedErrorMessage />
-    ) : (
-      _.get(err, 'response.data.message', err.message)
-    );
-
-    this.setState({
-      errorMessage
-    });
-  };
-
-  render() {
-    const { students, isLoading, errorMessage } = this.state;
-
-    if (isLoading) {
-      return <Spinner />;
-    }
-
-    return (
-      <div>
-        <ErrorMessage
-          isShow={!!errorMessage}
-          errorMessage={errorMessage}
-          onClose={this.onErrorClose}
-        />
-
-        <StudentsList students={students} />
-      </div>
-    );
-  }
+        return <StudentsList students={presentStudents} />;
+      }}
+    </GetStudents>
+  );
 }
 
 export default LessonAttendance;
